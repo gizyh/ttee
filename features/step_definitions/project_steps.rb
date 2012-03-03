41ce47c12a8c64a239e16ad68288210ea5157ec8
gizyh <chfxh1@gmail.com>
2012-03-03 22:08:07 +0800

    Add validation to ensure names are specified when creating projects

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,13 +1,18 @@
 Given /^I am on the homepage$/ do
   visit('/')
 end
-When /^I navigate to the new project creation page$/ do
-  click_link('New Project')
+When /^I follow "([^\"]*)"$/ do |arg1|
+  click_link(arg1)
 end
-When /^I create a new project$/ do
-  fill_in('Name', :with => 'TextMate 2')
-  click_button('Create Project') 
+When /^I fill in "([^\"]*)" with "([^\"]*)"$/ do |arg1, arg2|
+  fill_in(arg1, :with => arg2)
 end
-Then /^I should be shown the project created verification$/ do
-  page.should have_content("Project has been created.")
+When /^I press "([^\"]*)"$/ do |arg1|
+  click_button(arg1) 
+end
+Then /^I should see "([^\"]*)"$/ do |arg1|
+  page.should have_content(arg1)
+end
+Then /^I should be on the project page for "([^\"]*)"$/ do |arg1|
+  current_path.should == project_path(Project.find_by_name!(arg1))
 end
