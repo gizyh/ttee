08a2e4a4ef1c72e40df61f58bdcdc7615bfc0a65
gizyh <chfxh1@gmail.com>
2012-03-05 18:00:17 +0800

    Users should be redirected back to the projects page if they try going to a project that doesn't exist.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only =>[:show,:edit,:update,:destroy]
+
   def index
   	@projects = Project.all
   end
@@ -36,4 +38,20 @@ class ProjectsController < ApplicationController
   	  render :action => "edit"
   	end
   end
+
+  def destroy
+  	@project = Project.find(params[:id])
+  	@project.destroy
+  	flash[:notice] = "Project has been deleted."
+  	redirect_to projects_path
+  end
+
+	private
+	  def find_project
+	  	@project = Project.find(params[:id])
+	  rescue ActiveRecord::RecordNotFound
+	  	flash[:alert] = "The project you were looking for could not be found."
+	  	redirect_to projects_path
+	  end
 end
+
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -13,6 +13,9 @@ end
 Then /^I should see "([^\"]*)"$/ do |arg1|
   page.should have_content(arg1)
 end
+Then /^I should not see "([^\"]*)"$/ do |arg1|
+  page.should_not have_content(arg1)
+end
 Then /^I should be on the project page for "([^\"]*)"$/ do |arg1|
   current_path.should == project_path(Project.find_by_name!(arg1))
 end
