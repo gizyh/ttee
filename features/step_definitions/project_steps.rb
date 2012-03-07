3672a1cbad6512a1412006cc790a25e237c837e1
gizyh <chfxh1@gmail.com>
2012-03-07 18:31:27 +0800

    Implemented features for displaying a list of relevant tickets for projects and viewing particular tickets

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -20,5 +20,5 @@ Then /^I should be on the project page for "([^\"]*)"$/ do |arg1|
   current_path.should == project_path(Project.find_by_name!(arg1))
 end
 Given /^there is a project called "([^\"]*)"$/ do |name|
-  Factory(:project, :name => name)
+  @project = Factory(:project, :name => name)
 end
