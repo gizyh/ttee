22825ed05efaeff7f418aac120d281ba24cc00b2
gizyh <chfxh1@gmail.com>
2012-03-30 13:52:49 +0800

    Restrict actions in TicketsController based on permissions and hide links

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -4,7 +4,7 @@ Given /^that project has a ticket:$/ do |table|
   end
 end
 
-Given /^"([^\"]*)" has created a ticket for this porject:$/ do |email, table|
+Given /^"([^\"]*)" has created a ticket for this project:$/ do |email, table|
   table.hashes.each do |attributes|
   	attributes = attributes.merge!(:user => User.find_by_email!(email))
   	@project.tickets.create!(attributes)
